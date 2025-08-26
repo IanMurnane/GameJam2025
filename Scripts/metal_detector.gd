3bc9968a4f9ff6da80ede69fd346f877682d69d1
Ian Murnane <ian.murnane@fh-dortmund.de>
2025-08-26 22:54:26 +0200

    added metal detector sound

--- Scripts/metal_detector.gd
+++ Scripts/metal_detector.gd
@@ -1,9 +1,31 @@
 extends Node3D
 
+# Get references to the nodes
+@onready var area_detector: Area3D = $Area3D
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
-#func _on_area_3d_body_entered(body: Node3D) -> void:
-  #EventBus.player_entered.emit()
-#
-#
-#func _on_area_3d_body_exited(body: Node3D) -> void:
-  #EventBus.player_exited.emit()
+# A boolean to prevent the sound from spamming if the player stands still.
+var can_play_sound = true
+
+#func _ready() -> void:
+    # Connect the body_entered signal to our custom function.
+    #area_detector.body_entered.connect(on_body_entered)
+    # Also connect the body_exited signal to reset the flag.
+    #area_detector.body_exited.connect(on_body_exited)
+
+func on_body_entered(body: Node3D) -> void:
+    print("body entered")
+    # Check if the entered body is the player and the sound is not currently playing.
+    if body.is_in_group("player") and can_play_sound:
+        print("playing sound")
+        audio_player.play()
+        # Set the flag to false to prevent the sound from re-triggering immediately.
+        can_play_sound = false
+    else:
+        print("cant play sound ", body.is_in_group("player"), " ", can_play_sound)
+
+func on_body_exited(body: Node3D) -> void:
+    print("body exited")
+    # When the player leaves the area, reset the flag so the sound can be played again.
+    if body.is_in_group("player"):
+        can_play_sound = true
