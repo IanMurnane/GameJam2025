43d55025089b92f12a1470c6f03f9c8f82dde5de
Sachin Kumar <sachinkum123567@gmail.com>
2025-08-27 00:53:00 +0200

    revert collision shape

--- Scripts/pizza_shop.gd
+++ Scripts/pizza_shop.gd
@@ -3,11 +3,11 @@ extends Node3D
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
   # publish:
-  # EventBus.player_exited.emit()
+  EventBus.player_exited.emit()
   pass
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
   # publish:
-  # EventBus.player_entered.emit()
+  EventBus.player_entered.emit()
   pass
